143f5d1c358b2ffc62538bdb035e3e7b65cbcb3b
stanislaviw <slavi8ok@gmail.com>
2023-12-06 15:24:55 +0200

    fix bugs

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -1,27 +1,26 @@
 // import { ReactComponent as FormIcon } from "../assets/success-form.svg";
-
 import { useEffect, useState } from "react";
 import { CommonInput } from "../common/input";
 import { CommonButton } from "../common/button";
 import { CommonChechbox } from "../common/checkbox";
 
-const data = [
-  { id: 1, name: "Lawyer" },
-  { id: 2, name: "Content manager" },
-  { id: 3, name: "Security" },
-  { id: 4, name: "Designer" },
-];
+// const data = [
+//   { id: 1, name: "Lawyer" },
+//   { id: 2, name: "Content manager" },
+//   { id: 3, name: "Security" },
+//   { id: 4, name: "Designer" },
+// ];
 
 export const SignUp = () => {
   const [positions, setPositions] = useState([]);
-  const [selectedPosition, setSelectedPosition] = useState("");
+  const [selectedPosition, setSelectedPosition] = useState({});
 
-  // useEffect(() => {
-  //   fetch("https://frontend-test-assignment-api.abz.agency/api/v1/positions")
-  //     .then((response) => response.json())
-  //     .then((data) => setPositions(data))
-  //     .catch((err) => console.log(err));
-  // }, []);
+  useEffect(() => {
+    fetch("https://frontend-test-assignment-api.abz.agency/api/v1/positions")
+      .then((response) => response.json())
+      .then((data) => setPositions(data.positions))
+      .catch((err) => console.log(err));
+  }, []);
 
   console.log(positions);
 
@@ -40,11 +39,11 @@ export const SignUp = () => {
           <div className="sign-up_form_checkboxes">
             <p className="text_paragraph">Select your position</p>
             <div className="sign-up_form_checkboxes_box">
-              {data.map((position) => (
+              {positions.map((position) => (
                 <CommonChechbox
                   key={position.id}
                   label={position.name}
-                  checked={(position.name = selectedPosition.name)}
+                  checked={position.name === selectedPosition.name}
                   onChange={() => setSelectedPosition(position)}
                 />
               ))}
